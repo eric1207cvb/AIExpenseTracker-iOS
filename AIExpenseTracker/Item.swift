2dfc7a25a67f7edf9d809586bdce3d00f066538c
eric1207cvb <eric1207cvb@msn.com>
2025-09-02 10:57:07 +0800

    Initial Commit

---
+++ AIExpenseTracker/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  AIExpenseTracker
+//
+//  Created by Hsueh Yi-An on 2025/9/2.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ AIExpenseTrackerTests/AIExpenseTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIExpenseTrackerTests.swift
+//  AIExpenseTrackerTests
+//
+//  Created by Hsueh Yi-An on 2025/9/2.
+//
+
+import Testing
+@testable import AIExpenseTracker
+
+struct AIExpenseTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
